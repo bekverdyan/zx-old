793d78fa96f38f3d8d274c7e1b54aa2d50a4a752
Aram <aram.bekverdyan@gmail.com>
2019-10-07 19:21:47 +0400

    feat: accordion integration

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,18 @@
 module Main exposing (Model, init, main)
 
+import Bootstrap.Accordion as Accordion
 import Bootstrap.Alert as Alert
 import Bootstrap.Button as Button
+import Bootstrap.Card as Card
+import Bootstrap.Card.Block as Block
 import Bootstrap.Form as Form
 import Bootstrap.Form.Input as Input
 import Bootstrap.Form.InputGroup as InputGroup
 import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Col as Col
 import Bootstrap.Grid.Row as Row
+import Bootstrap.ListGroup as ListGroup
+import Bootstrap.Text as Text
 import Bootstrap.Utilities.Spacing as Spacing
 import Browser
 import Debug
@@ -23,7 +28,7 @@ main =
         { init = init
         , update = update
         , view = view
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
 
 
@@ -32,7 +37,9 @@ main =
 
 
 type alias Model =
-    { carwashes : Maybe (List Carwash) }
+    { carwashes : Maybe (List Carwash)
+    , accordionState : Accordion.State
+    }
 
 
 type Carwash
@@ -73,11 +80,11 @@ type alias WashboxCounter =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model Nothing, Cmd.none )
+    ( Model Nothing (Accordion.initialStateCardOpen "card1"), Cmd.none )
 
 
 type Msg
-    = SamuilArshak
+    = AccordionMsg Accordion.State
 
 
 
@@ -87,8 +94,8 @@ type Msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        SamuilArshak ->
-            ( model, Cmd.none )
+        AccordionMsg accordionState ->
+            ( { model | accordionState = accordionState }, Cmd.none )
 
 
 
@@ -97,4 +104,52 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    text "gago"
+    Accordion.config AccordionMsg
+        |> Accordion.withAnimation
+        |> Accordion.cards
+            [ Accordion.card
+                { id = "card1"
+                , options =
+                    [ Card.outlineSuccess
+                    , Card.align Text.alignXsCenter
+                    ]
+                , header =
+                    Accordion.headerH3 []
+                        (Accordion.toggle [] [ text " Card 1" ])
+                        |> Accordion.prependHeader
+                            [ span [ class "fa fa-car" ] [] ]
+                , blocks =
+                    [ Accordion.block [ Block.align Text.alignXsLeft ]
+                        [ Block.titleH4 [] [ text "Block title" ]
+                        , Block.text [] [ text "Lorem ipsum etc" ]
+                        ]
+                    , Accordion.block [ Block.align Text.alignXsRight ]
+                        [ Block.titleH4 [] [ text "Block2 title" ]
+                        , Block.text [] [ text "Lorem ipsum etc" ]
+                        ]
+                    ]
+                }
+            , Accordion.card
+                { id = "card2"
+                , options = [ Card.outlineSuccess, Card.align Text.alignXsCenter ]
+                , header =
+                    Accordion.headerH3 []
+                        (Accordion.toggle [] [ text " Card 2" ])
+                        |> Accordion.prependHeader
+                            [ span [ class "fa fa-taxi" ] [] ]
+                , blocks =
+                    [ Accordion.block []
+                        [ Block.text [] [ text "Lorem ipsum etc" ] ]
+                    , Accordion.listGroup
+                        [ ListGroup.li [] [ text "List item 1" ]
+                        , ListGroup.li [] [ text "List item 2" ]
+                        ]
+                    ]
+                }
+            ]
+        |> Accordion.view model.accordionState
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Accordion.subscriptions model.accordionState SamuilArshak
