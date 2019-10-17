ba293c438a75b1cb15ec20d4b33c2e0f9a63e773
Aram <aram.bekverdyan@gmail.com>
2019-10-17 18:34:14 +0400

    feat: Branch with device labales list types and view

--- src/Branch.elm
+++ src/Branch.elm
@@ -1,6 +1,25 @@
-module Branch exposing (CarwashMsg(..))
+module Branch exposing (Model, Msg(..), view)
 
 import Bootstrap.Accordion as Accordion
+import Bootstrap.Button as Button
+import Bootstrap.Card as Card
+import Bootstrap.Card.Block as Block
+import Bootstrap.Grid as Grid
+import Bootstrap.ListGroup as ListGroup
+import Bootstrap.Text as Text
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { carwashes : List Carwash
+    , accordionState : Accordion.State
+    }
 
 
 type alias Carwash =
@@ -18,12 +37,57 @@ type alias Label =
     }
 
 
-type CarwashMsg
-    = Smbulik String
-    | Samazvan Int
-    | Lolik
-
-
 type Msg
     = AccordionMsg Accordion.State
     | OnDeviceSelect String
+
+
+
+-- TODO Implement http receiver
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    viewCarwashes model.carwashes model.accordionState
+
+
+viewCarwashes : List Carwash -> Accordion.State -> Html Msg
+viewCarwashes carwashes accordionState =
+    Grid.container []
+        [ Grid.row []
+            [ Grid.col []
+                [ Accordion.config
+                    AccordionMsg
+                    |> Accordion.withAnimation
+                    |> Accordion.cards (List.map viewCarwash carwashes)
+                    |> Accordion.view accordionState
+                ]
+            ]
+        ]
+
+
+viewCarwash : Carwash -> Accordion.Card Msg
+viewCarwash carwash =
+    Accordion.card
+        { id = carwash.id
+        , options = [ Card.outlineSuccess, Card.align Text.alignXsCenter ]
+        , header =
+            Accordion.headerH3 []
+                (Accordion.toggle [] [ text carwash.name ])
+                |> Accordion.prependHeader
+                    [ span [ class "fa fa-taxi" ] [] ]
+        , blocks =
+            [ Accordion.listGroup <| List.map viewDeviceLabel carwash.devices ]
+        }
+
+
+viewDeviceLabel : Label -> ListGroup.Item Msg
+viewDeviceLabel device =
+    ListGroup.li []
+        [ Button.button
+            [ Button.light
+            , Button.attrs [ onClick <| OnDeviceSelect device.id ]
+            ]
+            [ text device.name ]
+        ]
