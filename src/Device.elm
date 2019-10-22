f5272cfc0bc997695edae74fd15d39ee682d51ae
Aram <aram.bekverdyan@gmail.com>
2019-10-22 18:58:46 +0400

    feat: device skeleton

--- src/Device.elm
+++ src/Device.elm
@@ -1,9 +1,10 @@
 module Device exposing (Model)
 
 import Bootstrap.Tab as Tab
+import Bootstrap.Utilities.Spacing as Spacing
+import Exchange
 import Html exposing (..)
-import Html.Attributes
-import Html.Events
+import Washbox
 
 
 
@@ -18,62 +19,14 @@ type alias Model =
 
 type alias Device =
     { info : String
-    , config : Config
-    , counter : Counter
+    , device : Type
     , log : Int
     }
 
 
-type Config
-    = Washbox Int (List Channel)
-    | Exchange Int Int
-
-
-type alias Counter =
-    { channelId : Int
-    , component : Component
-    , pointer : Int
-    }
-
-
-type alias ExchangeConfig =
-    { coinNominal : Int
-    , hopperCoinNominal : Int
-    }
-
-
-type alias ExchangeCounter =
-    { billCash : Int
-    , hopperCoin : Int
-    }
-
-
-type alias WashboxConfig =
-    { definedChannels : List Channel
-    , actualChannels : Int
-    }
-
-
-type alias Channel =
-    { id : Int
-    , components : List Component
-    }
-
-
-type alias Component =
-    { type_ : String
-    , unit : String
-    }
-
-
-type alias WashboxCounter =
-    List CounterData
-
-
-type alias CounterData =
-    { channelId : Int
-    , pointer : Int
-    }
+type Type
+    = Savok Exchange.Counters
+    | Exchangeik Washbox.Channels
 
 
 
@@ -82,16 +35,35 @@ type alias CounterData =
 
 type Msg
     = ChangeConfig
+    | TabMsg Tab.State
 
 
 
 -- VIEW
 
 
-view : Model -> Html Msg
-view model =
+view : Model -> Tab.State -> Html Msg
+view model tabState =
     let
         device =
             model.device
     in
-    text device.info
+    Tab.config
+        TabMsg
+        |> Tab.items
+            [ Tab.item
+                { id = "counters"
+                , link = Tab.link [] [ text "Counters" ]
+                , pane =
+                    Tab.pane [ Spacing.mt3 ]
+                        [ text "counters" ]
+                }
+            , Tab.item
+                { id = "settings"
+                , link = Tab.link [] [ text "Settings" ]
+                , pane =
+                    Tab.pane [ Spacing.mt3 ]
+                        [ text "configs" ]
+                }
+            ]
+        |> Tab.view tabState
